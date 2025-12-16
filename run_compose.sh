59c5fc87688675bb527fd36ff204d01ae7c10a33
uknown <cstc@HoneypotVM.tail116fbf.ts.net>
2025-12-16 23:57:02 +0000

    promtail done

--- run_compose.sh
+++ run_compose.sh
@@ -7,6 +7,7 @@ read x
 if [ "$x" -eq 1 ]; then
     docker compose up --build -d
     ./sql-h-log.sh 
+    ./run_promtail
 elif [ "$x" -eq 2 ]; then
     docker compose down
     # Kill the log streaming process
@@ -14,6 +15,12 @@ elif [ "$x" -eq 2 ]; then
         kill $(cat /tmp/mysql_honeypot_log.pid) 2>/dev/null
         rm /tmp/mysql_honeypot_log.pid
     fi
+    
+
+   if [ -f /tmp/promtail.pid ]; then 
+	kill $(cat /tmp/promtail.pid) 2>/dev/null
+	rm /tmp/promtail.pid
+   fi
 else
     echo "Invalid option"
 fi
